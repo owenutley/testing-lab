b1327ef8ae6e211a6b2be0f91720e51c2c438e7e
Owen Utley <owen.utley17@gmail.com>
2021-10-19 15:18:45 -0600

    fixed bugs

--- function.test.js
+++ function.test.js
@@ -10,8 +10,8 @@ it("Should return the greeting hello name", () => {
 
 describe("Math", () => {
 
-    it("Should return the math of the numbers", () => {
-        expect(add("bob", 6)).toEqual(11)
+    test("Should return the math of the numbers", () => {
+        expect(add(5, 6)).toEqual(11)
         expect(multiply(5, 6)).toEqual(30)
         expect(divide(10, 5)).toEqual(2)
         expect(subtract(6, 4)).toEqual(2)
